d71c5814276b860706b94fac9942e6c98d4f297c
boynayon00002 <boynayon00002@gmail.com>
2021-02-05 19:54:28 +0600

    add slice splice

---
+++ slice-aplice.js
@@ -0,0 +1,10 @@
+const names=[12,46,15,77,33];
+
+const part=names.slice(1, 3);
+console.log(part);
+
+const removed=names.splice(2, 3);
+console.log(removed);
+console.log(names);
+const add=names.join( '');
+console.log(add);
